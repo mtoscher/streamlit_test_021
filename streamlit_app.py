2fdb2a88ca9cd7153199a9750f0d1c3089329e34
mtoscher <97234831+mtoscher@users.noreply.github.com>
2023-11-09 10:32:51 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit as st
 st.title("This is our amazing title")
 
 st.write("This needs to be updated with a poem or a joke.")
+
+colour = st.text_input("What is your favourite colour?")
+
+st.write("Your favourite colour is:", colour)
